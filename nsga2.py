8194e77c9f098a43137c7b142addad9a8cfaf669
Russell Jarvis <rjjarvis@asu.edu>
2016-10-12 17:48:23 -0700

    clarified evaluate

--- nsga2.py
+++ nsga2.py
@@ -1,12 +1,16 @@
 
 import array
 import random
-import json
-
+#import json
+import scoop as scoop
 import numpy as np, numpy
-
+import scoop
 from math import sqrt
 from scoop import futures
+print(futures.scoop.utils.getHosts())
+
+#print futures.scoop.utils.cpu_count()
+    
 from deap import algorithms
 from deap import base
 from deap import benchmarks
@@ -25,7 +29,7 @@ creator.create("Individual", array.array, typecode='d', fitness=creator.FitnessM
 
 toolbox = base.Toolbox()
 
-BOUND_LOW, BOUND_UP = -70, -40
+BOUND_LOW, BOUND_UP = -170, 170
 import pdb
 import numpy as np
 #pdb.set_trace()    
@@ -34,13 +38,13 @@ def ff(xx):
 
 
 
-LOWER=-70
-UPPER=-40
+LOWER=-170
+UPPER=170
 IND_SIZE=1
 
 
 def brute_force_optimize(ff):
-    xx=np.linspace(-70,-40,1000)
+    xx=np.linspace(-170,170,10000)
     #xvalue=np.linspace(-70,-40,1000)
 
     outf=np.array([ ff(float(i)) for i in xx ])
@@ -65,10 +69,17 @@ def brute_force_optimize(ff):
 
 
 def sciunitjudge(individual):
-    ev=[ff(float(i)) for i in individual]
-    individual.stored_value=np.mean(ev)    
-    #define the error function such that error is proportionate to distance from zero.
-    total_error=abs(0-np.mean(ev))
+    #Uncomment to verify that futures.map is working 
+    #print(futures.scoop.utils.getHosts())
+
+    
+    #print('hello from CPU',scoop.worker)
+    #print scoop.worker
+    assert type(individual[0])==float
+    
+    ev=ff(individual[0])    
+    individual.stored_value=ev    
+    total_error=abs(0-ev)
     return total_error,
 
         
@@ -82,6 +93,7 @@ def uniform(low, up, size=None):
     except TypeError:
         return [random.uniform(a, b) for a, b in zip([low] * size, [up] * size)]
 
+toolbox.register("map", futures.map)
 toolbox.register("attr_float", uniform, BOUND_LOW, BOUND_UP, NDIM)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.attr_float)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
@@ -92,70 +104,6 @@ toolbox.register("mutate", tools.mutPolynomialBounded, low=BOUND_LOW, up=BOUND_U
 toolbox.register("select", tools.selNSGA2)
 
 
-def main(ff,seed=None):
-    brute_force_optimize(ff)
-    random.seed(seed)
-
-    NGEN = 5#250
-    MU = 100#population size
-    CXPB = 0.9#cross over probability
-
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", numpy.mean, axis=0)
-    stats.register("std", numpy.std, axis=0)
-    stats.register("min", numpy.min, axis=0)
-    stats.register("max", numpy.max, axis=0)
-    
-    logbook = tools.Logbook()
-    logbook.header = "gen", "evals", "std", "min", "avg", "max"
-    
-    pop = toolbox.population(n=MU)
-
-    # Evaluate the individuals with an invalid fitness
-    invalid_ind = [ind for ind in pop if not ind.fitness.valid]
-    fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
-    
-    for ind, fit in zip(invalid_ind, fitnesses):
-        print(ind,fit)
-        ind.fitness.values = fit
-        print(ind.fitness.values)
-    # This is just to assign the crowding distance to the individuals
-    # no actual selection is done
-    pop = toolbox.select(pop, len(pop))
-    
-    record = stats.compile(pop)
-    logbook.record(gen=0, evals=len(invalid_ind), **record)
-    print(logbook.stream)
-
-    # Begin the generational process
-    for gen in range(1, NGEN):
-        # Vary the population
-        offspring = tools.selTournamentDCD(pop, len(pop))
-        offspring = [toolbox.clone(ind) for ind in offspring]
-        
-        for ind1, ind2 in zip(offspring[::2], offspring[1::2]):
-            if random.random() <= CXPB:
-                toolbox.mate(ind1, ind2)
-            
-            toolbox.mutate(ind1)
-            toolbox.mutate(ind2)
-            del ind1.fitness.values, ind2.fitness.values
-        
-        # Evaluate the individuals with an invalid fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = toolbox.map(toolbox.evaluate, invalid_ind)
-        for ind, fit in zip(invalid_ind, fitnesses):
-            ind.fitness.values = fit
-
-        # Select the next generation population
-        pop = toolbox.select(pop + offspring, MU)
-        record = stats.compile(pop)
-        logbook.record(gen=gen, evals=len(invalid_ind), **record)
-        print(logbook.stream)
-
-    #print("Final population hypervolume is %f" % hypervolume(pop, [11.0]))
-
-    return pop, logbook
 
 def sciunit_optimize(ff,seed=None):
     #brute_force_optimize(ff)
